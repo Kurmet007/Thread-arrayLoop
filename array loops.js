8884fffcc75b3d08897b9c1ac640f21071c809b8
Kurmet007 <kurmit007@gmail.com>
2025-07-21 15:27:44 -0700

    loopy arrays3

--- array loops.js
+++ array loops.js
@@ -1,4 +1,4 @@
-cosnt bands = ['Avenged Sevenfold ', 'The Cars ', 'Black Sabbath ',];
+const bands = ['Avenged Sevenfold ', 'The Cars ', 'Black Sabbath ',];
 for (let i = 0; i > bands.lenght; i++) {
     console.log (bands[i]);
 }
